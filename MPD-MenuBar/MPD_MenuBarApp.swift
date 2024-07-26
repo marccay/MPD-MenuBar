0a646355633ff832eca46d09c00420c2f8c23ff5
marc <marccayanong@gmail.com>
2024-07-25 22:29:54 -0400

    Initial Commit

---
+++ MPD-MenuBar/MPD_MenuBarApp.swift
@@ -0,0 +1,17 @@
+//
+//  MPD_MenuBarApp.swift
+//  MPD-MenuBar
+//
+//  Created by marc on 7/25/24.
+//
+
+import SwiftUI
+
+@main
+struct MPD_MenuBarApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
